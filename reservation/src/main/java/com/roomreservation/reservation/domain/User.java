60d01bb47a18d62a28ca7155c606a35e5dc6a75d
sy0419 <psy041912@gmail.com>
2025-08-10 15:38:58 +0900

    Update User entity to include user information and role.

--- reservation/src/main/java/com/roomreservation/reservation/domain/User.java
+++ reservation/src/main/java/com/roomreservation/reservation/domain/User.java
@@ -1,26 +1,33 @@
 package com.roomreservation.reservation.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
-    // 유저 고유 ID (자동 생성됨) Unique ID for each user (auto-generated)
+    // 고유 사용자 ID (자동 생성됨) Unique ID for each user (auto-generated)
     @Id
     @GeneratedValue
     private Long id;
+    
+    // User role (e.g., USER or ADMIN)
+    // 사용자의 역할 (예: 일반 사용자 USER, 관리자 ADMIN)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-    // 유저 이름 User name
+    // 사용자의 이름 User's name
     private String name;
     
-    // 유저 이메일 User email
+    // 사용자의 이메일 User's email
     private String email;
 
-    // 유저 패스워드 User password
+    // 사용자의 패스워드 User's password
     private String password;
 
-    // 유저 전화번호 User phone number
+    // 사용자의 전화번호 User's phone number
     private String phoneNumber;
 
     // 기본 생성자 (Spring JPA가 내부적으로 사용함) Default constructor (used internally by Spring JPA)
@@ -34,6 +41,14 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public String getName() {
