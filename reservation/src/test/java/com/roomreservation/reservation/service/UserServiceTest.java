7fd57a3492eef8f5a81f785abd1779fb7fce6d23
sy0419 <psy041912@gmail.com>
2025-08-14 17:24:29 +0900

    test: add unit tests for register, login, getAll, and getById methods.

---
+++ reservation/src/test/java/com/roomreservation/reservation/service/UserServiceTest.java
@@ -0,0 +1,136 @@
+package com.roomreservation.reservation.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+import com.roomreservation.reservation.domain.User;
+import com.roomreservation.reservation.repository.UserRepository;
+
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRegiserUser_Success() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("1234");
+
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.empty());
+        when(userRepository.save(user)).thenReturn(user);
+
+        User result = userService.registerUser(user);
+
+        assertNotNull(result);
+        assertEquals("test@example.com", result.getEmail());
+
+        verify(userRepository).findByEmail("test@example.com");
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    void testLoginUser_Success() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("1234");
+
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
+        
+        User login = userService.login("test@example.com", "1234");
+
+        assertNotNull(login);
+        
+        assertEquals("test@example.com", login.getEmail());
+        assertEquals("1234", login.getPassword());
+
+        verify(userRepository).findByEmail("test@example.com");
+    }
+
+    @Test
+    void testLoginUserNotFind_failure() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("1234");
+
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.empty());
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.login("test@example.com", "1234");
+        });
+
+        assertEquals("Email is not exist. Please, Create an account.", exception.getMessage());
+
+        verify(userRepository).findByEmail("test@example.com");
+    }
+
+    @Test
+    void testLoginUserUncorrectPassword_failure() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("1234");
+
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            userService.login("test@example.com", "12345");
+        });
+
+        assertEquals("Password is not correct.", exception.getMessage());
+
+        verify(userRepository).findByEmail("test@example.com");
+    }
+
+    @Test
+    void testGetAllUsers() {
+        List<User> userList = new ArrayList<>();
+        User user1 = new User();
+        user1.setEmail("test@example.com");
+        user1.setPassword("1234");
+        userList.add(user1);
+
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> results = userService.getAllUsers();
+        User firstUser = results.get(0);
+
+        assertEquals("test@example.com", firstUser.getEmail());
+        assertEquals("1234", firstUser.getPassword());
+
+        verify(userRepository).findAll();
+    }
+
+    @Test
+    void getUserById_ShouldReturnUser_WhenIdExists() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setPassword("1234");
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user)); 
+
+        User results = userService.getUserById(1L);
+        assertEquals("test@example.com", results.getEmail());
+        assertEquals("1234", results.getPassword());
+
+        verify(userRepository).findById(1L);
+    }
+}
